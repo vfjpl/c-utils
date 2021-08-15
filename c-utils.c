76f52aa41f2f2c24f4638c1d4a5a155a4d191425
vfjpl <cosiekvfj@o2.pl>
2021-08-15 10:16:45 +0200

    Update c-utils.c

--- c-utils.c
+++ c-utils.c
@@ -1,10 +1,27 @@
 #include <string.h>
+#include <stdio.h>
+
+void util_strcpy(char* dest, const char* src, size_t size)
+{
+	size_t len = strnlen(src, size - 1);
+	memcpy(dest, src, len);
+	dest[len] = '\0';
+}
 
 void util_strncpy(char* dest, const char* src, size_t size)
 {
 	--size;
 	strncpy(dest, src, size);
 	dest[size] = '\0';
+}
+
+size_t util_strlcpy(char* dest, const char* src, size_t size)
+{
+	size_t ret = strlen(src);
+	size_t len = (ret >= size) ? size - 1 : ret;
+	memcpy(dest, src, len);
+	dest[len] = '\0';
+	return ret;
 }
 
 char* util_stpncpy(char* dest, const char* src, size_t size)
