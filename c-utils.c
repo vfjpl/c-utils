fa93585ddfd4ff5047a753fa33a45ffdbe012e97
vfjpl <cosiekvfj@o2.pl>
2021-08-15 11:16:43 +0200

    Update c-utils.c

--- c-utils.c
+++ c-utils.c
@@ -1,11 +1,24 @@
 #include <string.h>
 #include <stdio.h>
 
-void util_strncpy(char* dest, const char* src, size_t size)
+void util_strcpy_n(char* dest, const char* src, size_t size)
 {
 	--size;
 	strncpy(dest, src, size);
 	dest[size] = '\0';
+}
+
+char* util_strcpy_pn(char* dest, const char* src, size_t size)
+{
+	--size;
+	char* ret = stpncpy(dest, src, size);
+	dest[size] = '\0';
+	return ret;
+}
+
+size_t util_strcpy_ln(char* dest, const char* src, size_t size)
+{
+	return util_strcpy_pn(dest, src, size) - dest;
 }
 
 void util_fputs(const char* str, FILE* stream)
