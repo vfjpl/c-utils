3eec78fde3eaa89441ebec49f8dbe06cb02f190c
vfjpl <cosiekvfj@o2.pl>
2021-08-15 10:51:10 +0200

    Update c-utils.c

--- c-utils.c
+++ c-utils.c
@@ -8,14 +8,6 @@ void util_strncpy(char* dest, const char* src, size_t size)
 	dest[size] = '\0';
 }
 
-char* util_stpncpy(char* dest, const char* src, size_t size)
-{
-	--size;
-	char* ret = stpncpy(dest, src, size);
-	dest[size] = '\0';
-	return ret;
-}
-
 void util_fputs(const char* str, FILE* stream)
 {
 	fputs(str, stream);
