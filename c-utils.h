997c23b48941eee6109593c9487fe980a028d45a
vfjpl <cosiekvfj@o2.pl>
2021-09-18 18:17:14 +0200

    Update c-utils.h

--- c-utils.h
+++ c-utils.h
@@ -1,14 +1,6 @@
 #ifndef C-UTILS_H_INCLUDED
 #define C-UTILS_H_INCLUDED
 
-#include <sys/times.h>
-#include <sys/time.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <time.h>
-
 uint32_t util_atou32(const char* str);
 
 size_t util_strcpy_l(char* dest, const char* src);
