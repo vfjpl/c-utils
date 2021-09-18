9e2704ac3cb59da1f2f426963f48cfc98dc78c08
vfjpl <cosiekvfj@o2.pl>
2021-09-18 15:05:39 +0200

    Create c-utils.h

---
+++ c-utils.h
@@ -0,0 +1,18 @@
+#ifndef C-UTILS_H_INCLUDED
+#define C-UTILS_H_INCLUDED
+
+uint32_t util_atou32(const char* str);
+
+size_t util_strcpy_l(char* dest, const char* src);
+void util_strcpy_n(char* dest, const char* src, size_t size);
+char* util_strcpy_pn(char* dest, const char* src, size_t size);
+size_t util_strcpy_ln(char* dest, const char* src, size_t size);
+
+char* util_read_file(const char* name);
+void util_fputs(const char* str, FILE* stream);
+void util_puts(const char* str);
+
+uint32_t util_time_u32(void);
+struct timeval util_gettimeofday(void);
+
+#endif // C-UTILS_H_INCLUDED
