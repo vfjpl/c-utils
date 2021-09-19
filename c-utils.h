f559f31ed8f8516ea66de30c7a317b49401eee93
vfjpl <cosiekvfj@o2.pl>
2021-09-19 08:03:23 +0200

    Update c-utils.h

--- c-utils.h
+++ c-utils.h
@@ -1,19 +1,33 @@
 #ifndef C-UTILS_H_INCLUDED
 #define C-UTILS_H_INCLUDED
 
+
+/// stdlib
+//atoi
 uint32_t util_atou32(const char* str);
 
+
+/// string
+//strcpy
+//stpcpy
 size_t util_strcpy_l(char* dest, const char* src);
 void util_strcpy_n(char* dest, const char* src, size_t size);
 char* util_strcpy_np(char* dest, const char* src, size_t size);
 size_t util_strcpy_nl(char* dest, const char* src, size_t size);
 
+
+/// stdio
 char* util_read_file(const char* name);
+//fputs
 void util_fputs(const char* str, FILE* stream);
+//puts
 void util_puts(const char* str);
 
+
+/// time
 uint32_t util_time_u32(void);
-clock_t util_clock_monotonic(void);
 struct timeval util_gettimeofday(void);
+clock_t util_clock_monotonic(void);
+
 
 #endif // C-UTILS_H_INCLUDED
