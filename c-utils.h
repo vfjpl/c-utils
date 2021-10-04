d26dfa0a71db435ed3659c2cc81248436a7594ff
vfjpl <cosiekvfj@o2.pl>
2021-10-04 21:52:41 +0200

    Update c-utils.h

--- c-utils.h
+++ c-utils.h
@@ -10,13 +10,10 @@
 
 
 //! stdlib
-//atoi
 uint32_t util_atou32(const char* str);
 
 
 //! string
-//strcpy
-//stpcpy
 size_t util_strcpy_l(char* dest, const char* src);
 void util_strcpy_n(char* dest, const char* src, size_t size);
 char* util_strcpy_np(char* dest, const char* src, size_t size);
@@ -25,9 +22,7 @@ size_t util_strcpy_nl(char* dest, const char* src, size_t size);
 
 //! stdio
 char* util_read_file(const char* name);
-//fputs
 void util_fputs(const char* str, FILE* stream);
-//puts
 void util_puts(const char* str);
 
 
