c45641ab5db6282cbd0e7ec61c2e7168c3a2e6c4
vfjpl <cosiekvfj@o2.pl>
2024-02-28 19:27:39 +0100

    Update c-utils.h

--- c-utils.h
+++ c-utils.h
@@ -1,8 +1,6 @@
 #ifndef C-UTILS_H_INCLUDED
 #define C-UTILS_H_INCLUDED
 
-#include <sys/time.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,9 +8,6 @@
 
 
 //! stdlib
-uint32_t util_atou32(const char* str);
-int util_div_upward(int x, int y);
-
 uint16_t util_load16(const void* ptr);
 uint32_t util_load32(const void* ptr);
 void util_store16(void* ptr, uint16_t val);
@@ -28,8 +23,6 @@ size_t util_strcpy_l(char* dest, const char* src);
 void util_strcpy_n(char* dest, const char* src, size_t size);
 char* util_strcpy_np(char* dest, const char* src, size_t size);
 size_t util_strcpy_nl(char* dest, const char* src, size_t size);
-
-const char* util_strafter(const char* str, const char* set);
 
 
 //! stdio
