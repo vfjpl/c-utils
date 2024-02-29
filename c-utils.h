658e9d1fa5eda15b754a4a8672b58e1eda5ff1bf
vfjpl <cosiekvfj@o2.pl>
2024-02-29 10:20:09 +0100

    Update c-utils.h

--- c-utils.h
+++ c-utils.h
@@ -12,12 +12,10 @@ uint16_t util_load16(const void* ptr);
 uint32_t util_load32(const void* ptr);
 void util_store16(void* ptr, uint16_t val);
 void util_store32(void* ptr, uint32_t val);
+void util_system(const char* format, ...);
 
 
 //! string
-void util_swab(const void* src, void* dest, ssize_t size);
-void* util_mempcpy(void* dest, const void* src, size_t size);
-
 char* util_strcpy_p(char* dest, const char* src);
 size_t util_strcpy_l(char* dest, const char* src);
 void util_strcpy_n(char* dest, const char* src, size_t size);
