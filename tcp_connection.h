8d751693aaec57d44ee701d24d2d65d099b80974
vfjpl <vfjpl1@gmail.com>
2025-03-13 15:57:13 +0100

    cleanup

--- tcp_connection.h
+++ tcp_connection.h
@@ -1,7 +1,7 @@
 #ifndef TCP_CONNECTION_H_INCLUDED
 #define TCP_CONNECTION_H_INCLUDED
 
-#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int tcp_server_create(uint16_t port);
 int tcp_server_accept(int server_fd, struct sockaddr* addr, socklen_t* addrlen);
