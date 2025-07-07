a5ea4c36d2b35c3d44f473d331a595579adf90d6
vfjpl <vfjpl1@gmail.com>
2025-07-07 22:56:37 +0200

    cleanup utils

--- utils.c
+++ utils.c
@@ -14,7 +14,7 @@
 
 typedef struct
 {
-	char* buf;
+	char* ptr;
 	long size;
 } string_t;
 
@@ -37,13 +37,21 @@ void util_store32(void* ptr, uint32_t val)
 {
 	memcpy(ptr, &val, sizeof(val));
 }
+void util_copy16(void* dest, const void* src);
+{
+	memcpy(dest, src, sizeof(uint16_t));
+}
+void util_copy32(void* dest, const void* src);
+{
+	memcpy(dest, src, sizeof(uint32_t));
+}
 
 
 float util_atof(const char* str)
 {
 	return strtof(str, NULL);
 }
-uint32_t util_atou(const char* str)
+uint32_t util_atou32(const char* str)
 {
 	return strtoul(str, NULL, 0);
 }
@@ -57,21 +65,21 @@ size_t util_strcpy_l(char* dest, const char* src)
 {
 	return util_strcpy_p(dest, src) - dest;
 }
-void util_strcpy_n(char* dest, const char* src, size_t size)
+void util_strcpy_n(char* dest, const char* src, size_t n)
 {
-	--size;
-	strncpy(dest, src, size)[size] = '\0';
+	--n;
+	strncpy(dest, src, n)[n] = '\0';
 }
-char* util_strcpy_np(char* dest, const char* src, size_t size)
+char* util_strcpy_np(char* dest, const char* src, size_t n)
 {
-	--size;
-	char* end = stpncpy(dest, src, size);
-	dest[size] = '\0';
+	--n;
+	char* end = stpncpy(dest, src, n);
+	dest[n] = '\0';
 	return end;
 }
-size_t util_strcpy_nl(char* dest, const char* src, size_t size)
+size_t util_strcpy_nl(char* dest, const char* src, size_t n)
 {
-	return util_strcpy_np(dest, src, size) - dest;
+	return util_strcpy_np(dest, src, n) - dest;
 }
 
 
@@ -79,9 +87,9 @@ bool util_streq(const char* str1, const char* str2)
 {
 	return !strcmp(str1, str2);
 }
-bool util_strneq(const char* str1, const char* str2, size_t size)
+bool util_streq_n(const char* str1, const char* str2, size_t n)
 {
-	return !strncmp(str1, str2, size);
+	return !strncmp(str1, str2, n);
 }
 const char* util_strafter(const char* str, const char* delim)
 {
@@ -91,17 +99,17 @@ const char* util_strafter(const char* str, const char* delim)
 }
 
 
-void util_swab(const void* src, void* dest, ssize_t size)
+void util_bzero(void* dest, size_t n)
 {
-	swab(src, dest, size);
+	bzero(dest, n);
 }
-void* util_mempcpy(void* dest, const void* src, size_t size)
+void util_swab(const void* src, void* dest, ssize_t n)
 {
-	return mempcpy(dest, src, size);
+	swab(src, dest, n);
 }
-void util_bzero(void* dest, size_t size)
+void* util_mempcpy(void* dest, const void* src, size_t n)
 {
-	bzero(dest, size);
+	return mempcpy(dest, src, n);
 }
 
 
@@ -124,7 +132,7 @@ string_t util_asprintf(const char* format, ...)
 	va_list args;
 	va_start(args, format);
 	string_t str = {0};
-	str.size = vasprintf(&str.buf, format, args);
+	str.size = vasprintf(&str.ptr, format, args);
 	va_end(args);
 	return str;
 }
@@ -136,9 +144,9 @@ string_t util_readfile(const char* filename)
 	fseek(file, 0, SEEK_END);
 	str.size = ftell(file);
 	rewind(file);
-	str.buf = (char*)malloc(str.size + 1);
-	fread(str.buf, 1, str.size, file);
-	str.buf[str.size] = '\0';
+	str.ptr = (char*)malloc(str.size + 1);
+	fread(str.ptr, 1, str.size, file);
+	str.ptr[str.size] = '\0';
 	fclose(file);
 	return str;
 }
--- utils.h
+++ utils.h
@@ -7,7 +7,7 @@
 
 typedef struct
 {
-	char* buf;
+	char* ptr;
 	long size;
 } string_t;
 
@@ -15,23 +15,25 @@ uint16_t util_load16(const void* ptr);
 uint32_t util_load32(const void* ptr);
 void util_store16(void* ptr, uint16_t val);
 void util_store32(void* ptr, uint32_t val);
+void util_copy16(void* dest, const void* src);
+void util_copy32(void* dest, const void* src);
 
 float util_atof(const char* str);
-uint32_t util_atou(const char* str);
+uint32_t util_atou32(const char* str);
 
 char* util_strcpy_p(char* dest, const char* src);
 size_t util_strcpy_l(char* dest, const char* src);
-void util_strcpy_n(char* dest, const char* src, size_t size);
-char* util_strcpy_np(char* dest, const char* src, size_t size);
-size_t util_strcpy_nl(char* dest, const char* src, size_t size);
+void util_strcpy_n(char* dest, const char* src, size_t n);
+char* util_strcpy_np(char* dest, const char* src, size_t n);
+size_t util_strcpy_nl(char* dest, const char* src, size_t n);
 
 bool util_streq(const char* str1, const char* str2);
-bool util_strneq(const char* str1, const char* str2, size_t size);
+bool util_streq_n(const char* str1, const char* str2, size_t n);
 const char* util_strafter(const char* str, const char* delim);
 
-void util_swab(const void* src, void* dest, ssize_t size);
-void* util_mempcpy(void* dest, const void* src, size_t size);
-void util_bzero(void* dest, size_t size);
+void util_bzero(void* dest, size_t n);
+void util_swab(const void* src, void* dest, ssize_t n);
+void* util_mempcpy(void* dest, const void* src, size_t n);
 
 const char* util_strerror(void);
 
