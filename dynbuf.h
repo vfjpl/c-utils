d3222bcdb1de1684fea4f6edf5917135c6aad72b
vfjpl <vfjpl1@gmail.com>
2025-07-08 00:57:53 +0200

    cleanup

--- dynbuf.h
+++ dynbuf.h
@@ -4,14 +4,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 typedef struct
 {
 	void* ptr;
 	long size;
 } dynbuf_t;
 
-static dynbuf_t buf_append_impl(dynbuf_t dest, dynbuf_t src)
+dynbuf_t buf_append_impl(dynbuf_t dest, dynbuf_t src)
 {
 	dynbuf_t buf = {0};
 	buf.size = dest.size + src.size;
@@ -20,10 +19,10 @@ static dynbuf_t buf_append_impl(dynbuf_t dest, dynbuf_t src)
 	return buf;
 }
 
-#define buf_append_buf(dest, src)            dest = buf_append_impl(dest, src)
-#define buf_append_ptr_size(dest, ptr, size) buf_append_buf(dest, ((dynbuf_t){(void*)ptr, size}))
-#define buf_append_ptr(dest, ptr)            buf_append_ptr_size(dest, ptr, sizeof(*ptr))
-#define buf_append_type_val(dest, type, ...) buf_append_ptr(dest, &((type){__VA_ARGS__}))
+#define buf_append_buf(dest, src)             dest = buf_append_impl(dest, src)
+#define buf_append_ptr_size(dest, ptr, size)  buf_append_buf(dest, ((dynbuf_t){(void*)ptr, size}))
+#define buf_append_ptr(dest, ptr)             buf_append_ptr_size(dest, ptr, sizeof(*ptr))
+#define buf_append_type_val(dest, type, ...)  buf_append_ptr(dest, (&(type){__VA_ARGS__}))
 
 #define buf_index(b, t, i)                   *((t*)b.ptr + i)
 #define buf_elems(b, t)                      (b.size / sizeof(t))
@@ -41,6 +40,7 @@ EXAMPLE:
 	buf_append_type_val(buf1, int, 420);
 	buf_append_ptr_size(buf1, tab1, sizeof(tab1));
 	buf_append_type_val(buf1, int, 1337);
+	buf_append_type_val(buf1, int[], 1, 3, 4, 6, 3, 4);
 	for(long i = 0; i < buf_elems(buf1, int); ++i)
 		printf("%d\n", buf_index(buf1, int, i));
 	buf_free(buf1);
