220e7b63eefe3578f9a183bf87694cf09775a67b
vfjpl <vfjpl1@gmail.com>
2025-08-14 22:36:13 +0200

    Create types.h

---
+++ types.h
@@ -0,0 +1,16 @@
+#ifndef TYPES_H_INCLUDED
+#define TYPES_H_INCLUDED
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#define COUNTOF(array) (sizeof(array) / sizeof(*array))
+
+typedef int8_t s8;
+typedef uint8_t u8;
+typedef int16_t s16;
+typedef uint16_t u16;
+typedef int32_t s32;
+typedef uint32_t u32;
+
+#endif // TYPES_H_INCLUDED
