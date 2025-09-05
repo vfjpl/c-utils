26220ddd3eb1cb22063e4bac9d71ce8f56aae5ec
vfjpl <vfjpl1@gmail.com>
2025-09-05 11:15:40 +0200

    update types

--- types.h
+++ types.h
@@ -3,8 +3,10 @@
 
 #include <stdbool.h>
 #include <stdint.h>
+#include <assert.h>
 
-#define COUNTOF(array) (sizeof(array) / sizeof(*array))
+#define COUNTOF(array) (sizeof(array)/sizeof(*array))
+#define STATIC_ASSERT(...)
 
 typedef int8_t s8;
 typedef uint8_t u8;
@@ -12,5 +14,7 @@ typedef int16_t s16;
 typedef uint16_t u16;
 typedef int32_t s32;
 typedef uint32_t u32;
+
+typedef unsigned long ulong;
 
 #endif // TYPES_H_INCLUDED
