439e914180a5b398ddedbc0c6baa189c6f4438e4
vfjpl <vfjpl1@gmail.com>
2025-10-03 14:34:11 +0200

    update types

--- types.h
+++ types.h
@@ -1,20 +1,17 @@
 #ifndef TYPES_H_INCLUDED
 #define TYPES_H_INCLUDED
 
+#include <sys/param.h>
 #include <stdbool.h>
-#include <stdint.h>
-#include <assert.h>
+
+typedef unsigned long ulong;
 
 #define COUNTOF(array) (sizeof(array)/sizeof(*array))
 #define STATIC_ASSERT(...)
 
-typedef int8_t s8;
-typedef uint8_t u8;
-typedef int16_t s16;
-typedef uint16_t u16;
-typedef int32_t s32;
-typedef uint32_t u32;
-
-typedef unsigned long ulong;
+#define SETBIT(a,i) ((a) |= 1<<(i))
+#define CLRBIT(a,i) ((a) &= ~(1<<(i)))
+#define ISSET(a,i) (((a) & (1<<(i))) != 0)
+#define ISCLR(a,i) (((a) & (1<<(i))) == 0)
 
 #endif // TYPES_H_INCLUDED
