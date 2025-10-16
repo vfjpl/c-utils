d6cf4dd1b2fb50dfcefaa6b48331d91d0046011b
vfjpl <vfjpl1@gmail.com>
2025-10-16 18:14:31 +0200

    include pthread.h

--- pthread_utils.h
+++ pthread_utils.h
@@ -1,6 +1,8 @@
 #ifndef PTHREAD_UTILS_H_INCLUDED
 #define PTHREAD_UTILS_H_INCLUDED
 
-int pthread_create_detached(void* (*func)(void*), void* arg);
+#include <pthread.h>
+
+int pthread_create_detached(void*(*func)(void*), void* arg);
 
 #endif // PTHREAD_UTILS_H_INCLUDED
