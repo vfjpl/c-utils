aba01be5621f8013ec4f8d767fcc1c83a981166f
vfjpl <vfjpl1@gmail.com>
2025-12-20 17:52:27 +0100

    set ai_flags

--- tcp_utils.c
+++ tcp_utils.c
@@ -11,7 +11,7 @@ int tcp_client_connect(const char* host, const char* port)
 {
 	struct addrinfo* list = NULL;
 	struct addrinfo hints = {0};
-	//hints.ai_flags = AI_NUMERICSERV;
+	hints.ai_flags = 0;//AI_NUMERICSERV
 	hints.ai_family = PF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
